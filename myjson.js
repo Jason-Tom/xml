b52cffbc8e7390ba8b08bc59437f548540f7ce12
Jason-Tom <429295712@qq.com>
2016-06-05 18:34:24 +0800

    会定位读取服务器上的xml数据 但是不会发送上去

---
+++ myjson.js
@@ -0,0 +1,36 @@
+var a ={
+    subjects: [{
+        rate: "9.5",
+        cover_x: 4580,
+        is_beetle_subject: false,
+        title: "权力的游戏 第六季",
+        url: "https://movie.douban.com/subject/25862357/",
+        playable: false,
+        cover: "https://img1.doubanio.com/view/movie_poster_cover/lpst/public/p2321971908.jpg",
+        id: "25862357",
+        cover_y: 6784,
+        is_new: false
+    }, {
+        rate: "8.6",
+        cover_x: 606,
+        is_beetle_subject: false,
+        title: "余罪",
+        url: "https://movie.douban.com/subject/26363830/",
+        playable: false,
+        cover: "https://img3.doubanio.com/view/movie_poster_cover/lpst/public/p2355890225.jpg",
+        id: "26363830",
+        cover_y: 911,
+        is_new: false
+    }, {
+        rate: "8.0",
+        cover_x: 807,
+        is_beetle_subject: false,
+        title: "好运罗曼史",
+        url: "https://movie.douban.com/subject/26742362/",
+        playable: false,
+        cover: "https://img3.doubanio.com/view/movie_poster_cover/lpst/public/p2355341010.jpg",
+        id: "26742362",
+        cover_y: 1200,
+        is_new: true
+    }]
+}
